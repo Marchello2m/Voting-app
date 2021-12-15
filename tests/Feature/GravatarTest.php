2b797c76add0cab290c905903f4df66a0698c7b6
Mārtiņš <marchello2m@gmail.com>
2021-12-15 20:15:50 +0200

    "Search Filters ,Test"

--- tests/Feature/GravatarTest.php
+++ tests/Feature/GravatarTest.php
@@ -13,70 +13,78 @@ class GravatarTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-
     public function user_can_generate_gravatar_default_image_when_no_email_found_first_character_a()
     {
         $user = User::factory()->create([
-            'name' => 'Martins',
             'email' => 'afakeemail@fakeemail.com',
         ]);
+
         $gravatarUrl = $user->getAvatar();
+
         $this->assertEquals(
-            'https://www.gravatar.com/avatar/' . md5($user->email) . '?s=200&d=https://s3.amazonaws.com/laracasts/img/forum/avatars/default-avatar-1.png',
+            'https://www.gravatar.com/avatar/'.md5($user->email).'?s=200&d=https://s3.amazonaws.com/laracasts/images/forum/avatars/default-avatar-1.png',
             $gravatarUrl
         );
-        $response =Http::get($user->getAvatar());
+
+        $response = Http::get($user->getAvatar());
+
         $this->assertTrue($response->successful());
     }
 
     /** @test */
-
     public function user_can_generate_gravatar_default_image_when_no_email_found_first_character_z()
     {
         $user = User::factory()->create([
-            'name' => 'Martins',
             'email' => 'zfakeemail@fakeemail.com',
         ]);
+
         $gravatarUrl = $user->getAvatar();
+
         $this->assertEquals(
-            'https://www.gravatar.com/avatar/' . md5($user->email) . '?s=200&d=https://s3.amazonaws.com/laracasts/img/forum/avatars/default-avatar-26.png',
+            'https://www.gravatar.com/avatar/'.md5($user->email).'?s=200&d=https://s3.amazonaws.com/laracasts/images/forum/avatars/default-avatar-26.png',
             $gravatarUrl
         );
-        $response =Http::get($user->getAvatar());
+
+        $response = Http::get($user->getAvatar());
+
         $this->assertTrue($response->successful());
     }
 
     /** @test */
-
     public function user_can_generate_gravatar_default_image_when_no_email_found_first_character_0()
     {
         $user = User::factory()->create([
-            'name' => 'Martins',
             'email' => '0fakeemail@fakeemail.com',
         ]);
+
         $gravatarUrl = $user->getAvatar();
+
         $this->assertEquals(
-            'https://www.gravatar.com/avatar/' . md5($user->email) . '?s=200&d=https://s3.amazonaws.com/laracasts/img/forum/avatars/default-avatar-27.png',
+            'https://www.gravatar.com/avatar/'.md5($user->email).'?s=200&d=https://s3.amazonaws.com/laracasts/images/forum/avatars/default-avatar-27.png',
             $gravatarUrl
         );
-        $response =Http::get($user->getAvatar());
+
+        $response = Http::get($user->getAvatar());
+
         $this->assertTrue($response->successful());
     }
 
     /** @test */
-
     public function user_can_generate_gravatar_default_image_when_no_email_found_first_character_9()
     {
         $user = User::factory()->create([
-            'name' => 'Martins',
             'email' => '9fakeemail@fakeemail.com',
         ]);
+
         $gravatarUrl = $user->getAvatar();
+
         $this->assertEquals(
-            'https://www.gravatar.com/avatar/' . md5($user->email) . '?s=200&d=https://s3.amazonaws.com/laracasts/img/forum/avatars/default-avatar-36.png',
+            'https://www.gravatar.com/avatar/'.md5($user->email).'?s=200&d=https://s3.amazonaws.com/laracasts/images/forum/avatars/default-avatar-36.png',
             $gravatarUrl
         );
-        $response =Http::get($user->getAvatar());
+
+        $response = Http::get($user->getAvatar());
+
         $this->assertTrue($response->successful());
     }
 }
