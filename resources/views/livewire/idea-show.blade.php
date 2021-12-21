8a0d363a0a0f8eff6f19c3f9693186a6d090e981
Mārtiņš <marchello2m@gmail.com>
2021-12-21 10:20:26 +0200

    "Edit Idea with Policies"

--- resources/views/livewire/idea-show.blade.php
+++ resources/views/livewire/idea-show.blade.php
@@ -54,6 +54,7 @@
                                 @click.away="isOpen = false"
 
                             >
+                                @can('update',$idea)
                                 <li>
                                     <a
                                         href="#"
@@ -66,6 +67,7 @@
                                     >Edit Idea
                                     </a>
                                 </li>
+                                @endcan
                                 <li><a href="#" class="hover:bg-gray-100 block transition
                                 duration-150 ease-in px-5 py-3">Delete Idea</a></li>
                                 <li><a href="#" class="hover:bg-gray-100 block transition
