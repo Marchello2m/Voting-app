37e9d76f6274ee8e3e4acf39e6b965dd7de96688
Mārtiņš <marchello2m@gmail.com>
2021-12-22 10:50:46 +0200

    "Success Message Notification CSS"

--- resources/views/components/modal-confirm.blade.php
+++ resources/views/components/modal-confirm.blade.php
@@ -20,7 +20,7 @@
     x-init="window.livewire.on('{{ $eventToCloseModal }}',()=>{
       isOpen=false
     })"
-    class="fixed z-10 inset-0 overflow-y-auto"
+    class="fixed z-20 inset-0 overflow-y-auto"
     aria-labelledby="modal-title"
     role="dialog"
     aria-modal="true"
