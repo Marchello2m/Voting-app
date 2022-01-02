0dca869bbf34fcdca428ce058fea93611e561944
Mārtiņš <marchello2m@gmail.com>
2022-01-02 20:03:42 +0200

    "Edit Comments"

--- resources/views/components/modals-container.blade.php
+++ resources/views/components/modals-container.blade.php
@@ -1,16 +1,16 @@
-@can('update',$idea)
-    <livewire:edit-idea
-        :idea="$idea"
-    />
+@can('update', $idea)
+    <livewire:edit-idea :idea="$idea" />
 @endcan
-@can('delete',$idea)
+@can('delete', $idea)
     <livewire:delete-idea :idea="$idea" />
 @endcan
-
 @auth
     <livewire:mark-idea-as-spam :idea="$idea" />
 @endauth
-
 @admin
 <livewire:mark-idea-as-not-spam :idea="$idea" />
 @endadmin
+
+@auth
+    <livewire:edit-comment />
+@endauth
--- resources/views/livewire/idea-comment.blade.php
+++ resources/views/livewire/idea-comment.blade.php
@@ -1,25 +1,26 @@
 <div class="comment-container relative bg-white rounded-xl flex transition duration-500 ease-in mt-4">
-    <div class="flex flex-col md:flex-row flex-1 px-4 py-6">
-        <div class="flex-none">
-            <a href="#">
-                <img src="{{ $comment->user->getAvatar() }}" alt="avatar" class="w-14 h-14 rounded-xl">
-            </a>
+<div class="flex flex-col md:flex-row flex-1 px-4 py-6">
+    <div class="flex-none">
+        <a href="#">
+            <img src="{{ $comment->user->getAvatar() }}" alt="avatar" class="w-14 h-14 rounded-xl">
+        </a>
+    </div>
+    <div class="w-full md:mx-4">
+        <div class="text-gray-600">
+            {{ $comment->body }}
         </div>
-        <div class="w-full md:mx-4">
-            <div class="text-gray-600 ">
-                {{ $comment->body }}
-            </div>
-
-            <div class="flex items-center justify-between mt-6">
-                <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
-                    <div class="font-bold text-gray-900">{{ $comment->user->name }}</div>
+        <div class="flex items-center justify-between mt-6">
+            <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
+                <div class="font-bold text-gray-900">{{ $comment->user->name }}</div>
+                <div>&bull;</div>
+                {{-- @if ($comment->user->id === $comment->idea->user->id) --}}
+                @if ($comment->user->id === $ideaUserId)
+                    <div class="rounded-full border bg-gray-100 px-3 py-1">OP</div>
                     <div>&bull;</div>
-                    @if ($comment->user->id === $ideaUserId)
-                        <div class="rounded-full border bg-gray-100 px-3 py-1">OP</div>
-                        <div>&bull;</div>
-                    @endif
-                    <div>{{ $comment->created_at->diffForHumans() }}</div>
-                </div>
+                @endif
+                <div>{{ $comment->created_at->diffForHumans() }}</div>
+            </div>
+            @auth
                 <div
                     class="flex items-center space-x-2"
                     x-data="{ isOpen: false }"
@@ -38,12 +39,28 @@
                             @click.away="isOpen = false"
                             @keydown.escape.window="isOpen = false"
                         >
+                            @can('update', $comment)
+                                <li>
+                                    <a
+                                        href="#"
+                                        @click.prevent="
+                                        isOpen = false
+                                        Livewire.emit('setEditComment', {{ $comment->id }})
+                                        {{-- $dispatch('custom-show-edit-modal') --}}
+                                            "
+                                        class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3"
+                                    >
+                                        Edit Comment
+                                    </a>
+                                </li>
+                            @endcan
                             <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Mark as Spam</a></li>
                             <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Delete Post</a></li>
                         </ul>
                     </div>
                 </div>
-            </div>
+            @endauth
         </div>
     </div>
-</div> <!-- end comment-container -->
+</div>
+</div> <!-- end comm
